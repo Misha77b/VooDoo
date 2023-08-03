99751da9c1f6e7d0e15ecee0f77aaa98b6acf7d5
Misha <misha77b@gmail.com>
2023-08-03 18:19:33 +0300

    accordion bar withiut text inside

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+      },
       fontFamily: {
         "space-grotesk": ["Space Grotesk"],
         "space-mono": ["Space Mono"],
       },
       width: {
         "card-img": "300px",
+        accordion: "1200px",
       },
       height: {
         "card-img": "300px",
