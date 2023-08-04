60862293313a44da13f582b52b6081dcca9bba91
Misha <misha77b@gmail.com>
2023-08-04 22:55:18 +0300

    render cart items, but woith some problems

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,4 +1,5 @@
 const cart = document.getElementById("cart-btn");
+const shoppingCartList = document.getElementById("shopping-cart-list");
 const shoppingCartSidebar = document.getElementById("drawer-navigation");
 const closeShoppingCart = document.getElementById("close-shopping-cart");
 const cartCounter = document.getElementById("quantity");
@@ -48,6 +49,7 @@ class Cart {
       console.log(this.cartQuantity, cartCounter);
       console.log("Product added to cart:", productToAdd);
       this.shoppingCart.push(productToAdd);
+      this.renderShoppingCart(this.shoppingCart);
       console.log("cart shopping", this.shoppingCart);
     } else {
       console.error("Product not found.");
@@ -65,7 +67,48 @@ class Cart {
     cartCounter.innerText = this.cartQuantity;
   }
 
-  renderShoppingCart() {}
+  renderShoppingCart(shoppingCart) {
+    shoppingCart.forEach((product) => {
+      const cartCard = `<div class="flex w-full">
+        <img
+            class="w-[74px] h-[74px] mr-[18px] border border-[#FCF7E6] rounded"
+            src="./images/Logomark.png"
+            alt="product img"
+        />
+        <div class="flex flex-col justify-between text-xs font-bold">
+            <span>Products name</span>
+            <span>1 000 KR</span>
+            <span>- 1 +</span>
+        </div>
+        <button id="delete-from-cart-btn" class="self-start ml-auto hover:bg-[#3c3c3c] p-1.5 rounded-lg">
+            <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+            <g id="System-Line/delete-bin-6-line" clip-path="url(#clip0_2720_971)">
+                <g id="Group">
+                <path id="Vector"
+                    d="M7 4V2H17V4H22V6H20V21C20 21.2652 19.8946 21.5196 19.7071 21.7071C19.5196 21.8946 19.2652 22 19 22H5C4.73478 22 4.48043 21.8946 4.29289 21.7071C4.10536 21.5196 4 21.2652 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"
+                    fill="#FCF7E6"
+                />
+                </g>
+            </g>
+            <defs>
+                <clipPath id="clip0_2720_971">
+                <rect width="24" height="24" fill="white" />
+                </clipPath>
+            </defs>
+            </svg>
+        </button>
+        </div>`;
+      shoppingCart.length > 0
+        ? shoppingCartList.insertAdjacentHTML("beforeend", cartCard)
+        : (shoppingCartList.innerHTML = "<span>Cart is empty</span>");
+    });
+  }
 }
 
 const cartProducts = new Cart();
