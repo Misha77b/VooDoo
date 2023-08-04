8ba0fbe0677e3f0b50b7bc3ae0884c75c16a3dda
Misha <misha77b@gmail.com>
2023-08-04 19:15:43 +0300

    cart sidebar layout

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,11 +1,11 @@
-const cart = document.getElementById("cart-icon");
+const cart = document.getElementById("cart-btn");
 const shoppingCartSidebar = document.getElementById("drawer-navigation");
 const closeShoppingCart = document.getElementById("close-shopping-cart");
 
 cart.addEventListener("click", (e) => {
-  shoppingCartSidebar.classList.remove("translate-x-full");
+  shoppingCartSidebar.classList.toggle("translate-x-full");
 });
 
 closeShoppingCart.addEventListener("click", (e) => {
-  shoppingCartSidebar.classList.add("translate-x-full");
+  shoppingCartSidebar.classList.toggle("translate-x-full");
 });
